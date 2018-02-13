35b9b8c603b0be11969901c8ed5d23e6085f4568
jean <msjeanneramos@gmail.com>
2018-02-13 17:28:11 -0500

    light comment/spacing cleanup

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -3,11 +3,10 @@ import React from 'react';
 
 const Html = (props) => {
   // pull data from props for easier referencing
-  // <link rel="stylesheet" href="css/styles.css" />
   const data = props.initialData;
   const dataJSON = JSON.stringify(data);
   const helmet = props.helmet;
-  console.log('props helmet', props.helmet);
+
   return (
     <html className="no-js" lang="">
       <head>
@@ -19,10 +18,10 @@ const Html = (props) => {
         <div id="app">
           {props.children}
         </div>
-        <script id="initial-data" type="text/plain" data-json={dataJSON}></script>
-
-        <script src="http://localhost:3001/client.js"></script>
         
+        <script id="initial-data" type="text/plain" data-json={dataJSON}></script>
+        <script src="http://localhost:3001/client.js"></script>
+
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>  
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
