3c19a1b1d9838ac31638d0ce1207cccb491df25f
jean <msjeanneramos@gmail.com>
2018-02-14 13:08:37 -0500

    added enzyme adapter to root test file, then added first React component test for layout, and updated the descriptions of the two example tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,36 @@
 import test from 'ava';
+import React from 'react';
+import Enzyme from 'enzyme';
+import {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('foo', t => {
+// provides Enzyme compatibility with React 16
+Enzyme.configure({ adapter: new Adapter() });
+
+
+/* EXAMPLE TESTS (ASYNC AND SYNC) */
+
+test('Sync test example', t => {
     t.pass();
 });
 
-test('bar', async t => {
+test('Async test ezample', async t => {
     const bar = Promise.resolve('bar');
 
       t.is(await bar, 'bar');
+});
+
+
+/* REACT COMPONENT TESTS */
+
+// LAYOUT
+import Layout from '../src/components/Layout';
+
+test('Layout component renders children when passed in', t => {
+  const wrapper = shallow(
+      <Layout>
+        <div className="foo"/>
+      </Layout>
+  );
+  t.true(wrapper.contains(<div className="foo"/>));
 });
