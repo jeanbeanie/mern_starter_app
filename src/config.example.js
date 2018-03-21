6818468c53eeb53b794a383baa57e3e13ed3da6f
jean <msjeanneramos@gmail.com>
2018-03-21 11:50:59 -0400

    removed real config from repo and placed an example file to be used for demonstrative purposes

--- src/config.example.js
+++ src/config.example.js
@@ -1,9 +1,12 @@
-/* src/config.js */
-/* @flow */
+/* src/config.example.js */
 
-/* Add to .gitignore, do not commit this file. */
+/* Example config file, do not commit real config file!! */
 module.exports = {
   /* Default Metadata */
+  app: {
+    port: 1234,
+    rootUrl: 'http://localhost:1234',
+  },
   metaData: {
     metaTitle: 'MERN Vanilla Bean',
     metaKeywords: 'MERN, Mongo, Express, React, Node',
