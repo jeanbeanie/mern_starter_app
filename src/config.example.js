b23d500edf7f69c4eb8f5564dac84ded83fbeb20
jean <msjeanneramos@gmail.com>
2018-04-01 16:31:49 -0400

    updated example config file to match the current config requirements of the app

--- src/config.example.js
+++ src/config.example.js
@@ -5,9 +5,12 @@ module.exports = {
   /* Default Metadata */
   app: {
     port: 1234,
+    devPort: 2345,
     rootUrl: 'http://localhost:1234',
+    devRootUrl: 'http://localhost:2345',
   },
   metaData: {
+    title: 'MERN Vanilla Bean',
     metaTitle: 'MERN Vanilla Bean',
     metaKeywords: 'MERN, Mongo, Express, React, Node',
     metaDescription: 'Node Express React starter application.',
